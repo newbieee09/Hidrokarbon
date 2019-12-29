5b5a5c1ff611ea439d007423ac74523cbaba6217
newbieee09 <59330565+newbieee09@users.noreply.github.com>
2019-12-29 22:40:29 +0700

    Menambahkan pembatas

--- inputOutputHidrokarbon.cpp
+++ inputOutputHidrokarbon.cpp
@@ -8,6 +8,7 @@ int main(){
 	cout << "PROGRAM MENCARI RUMUS SENYAWA HIDROKARBON" << endl;
 	cout <<  "MASUKKAN JUMLAH ATOM KARBON (C) = "; cin >> c;
 	
+	cout << "=========================================================" << endl;
 	cout << "RUMUS SENYAWA ALKANA = " << "C" << c << "H" << 2*c + 2 << endl;
 	cout << "RUMUS SENYAWA ALKENA = " << "C" << c << "H" << 2*c << endl;
 	cout << "RUMUS SENYAWA ALKUNA = " << "C" << c << "H" << 2*c - 2 << endl;

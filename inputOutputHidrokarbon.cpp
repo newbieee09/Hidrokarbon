b7e379708ee159cda3ddf392ef8e8791e4010348
newbieee09 <vieriferdian124@gmail.com>
2020-01-02 16:36:46 +0700

    Menambahkan array alkena dan alkuna

--- inputOutputHidrokarbon.cpp
+++ inputOutputHidrokarbon.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main(){
 	
 	int c;
-	char* alkana[] = {"tidak diketahui", "Metana", "Etana", "Propana", "Butana", "Pentana", "Heksana", "Heptana", "Oktana", "Nonana", "Dekana"};
-	
+	char* alkana[] = {"-----", "Metana", "Etana", "Propana", "Butana", "Pentana", "Heksana", "Heptana", "Oktana", "Nonana", "Dekana"};
+	char* alkena[] = {"-----", "-----", "Etena", "Propena", "Butena", "Pentena", "Heksena", "Heptena", "Oktena", "Nonena", "Dekena"};
+	char* alkuna[] = {"-----", "-----", "Etuna", "Propuna", "Butuna", "Pentuna", "Heksuna", "Heptuna", "Oktuna", "Nonuna", "Dekuna"};
 	cout << "PROGRAM MENCARI RUMUS SENYAWA HIDROKARBON" << endl;
 	cout <<  "MASUKKAN JUMLAH ATOM KARBON (C) = "; cin >> c;
 				    
 	cout << "=========================================================" << endl;
-	cout << "RUMUS SENYAWA ALKANA = " << "C" << c << "H" << 2*c + 2 << " Nama senyawa : " << alkana[c] << endl;
-	cout << "RUMUS SENYAWA ALKENA = " << "C" << c << "H" << 2*c << endl;
-	cout << "RUMUS SENYAWA ALKUNA = " << "C" << c << "H" << 2*c - 2 << endl;
-	cout << "RUMUS SENYAWA ALKOHOL = " << "C" << c << "H" << 2*c + 2 << "O" << endl;
+	cout << "RUMUS SENYAWA ALKANA = " << "C" << c << "H" << 2*c + 2 << " ->" <<" Nama senyawa : " << alkana[c] << endl;
+	cout << "RUMUS SENYAWA ALKENA = " << "C" << c << "H" << 2*c << " ->" << " Nama senyawa : " << alkena[c] << endl;
+	cout << "RUMUS SENYAWA ALKUNA = " << "C" << c << "H" << 2*c - 2 << " ->" << " Nama senyawa : " << alkuna [c]<< endl;
 	cout << "=========================================================" << endl;
 }
